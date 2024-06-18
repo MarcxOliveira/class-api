b26ba424301a9b475a6a36f410eceb2e00a0853c
MarcxOliveira <marcoooliveira03@gmail.com>
2024-06-17 22:11:35 -0300

    feat: created the api directory to be an api consumption playground

---
+++ src/api/cep/index.jsx
@@ -0,0 +1,36 @@
+
+import React, {useState} from "react";
+import axios from "axios";
+
+export default function GetCep(){
+
+  const [cep, setCep] = useState("")
+  const [address, setAddress] = useState(null);
+  console.log(address) 
+
+  async function handleAddress() {
+    try{
+      const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+      setAddress(response.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return(
+    <div>
+      <h1>API busca CEP</h1>
+      <input type="text" onChange={(e) => setCep(e.target.value)} />
+      <button onClick={handleAddress} > Busque seu CEP! </button>
+
+      {address && (
+        <div>
+          <p>{address.logradouro}</p>
+          <p>{address.ddd}</p>
+          <p>{address.bairro}</p>
+          <p>{address.localidade}</p>
+        </div>
+      )}
+      </div>
+  )
+}
